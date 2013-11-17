72addadae68a7d1f024b1d5eac97e3e50e827c36
sput-nick <randolphdavidn@gmail.com>
2013-11-16 21:18:02 -0500

    Moved file to memory instead of reading from disk

--- swfdumper.c
+++ swfdumper.c
@@ -1,82 +1,80 @@
 
-// swfdump
-// dump information about a swf file
+// swfdumper
+// dump information from a file
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int idFile(FILE *inFile)
-{
-	// identify the file
-	int i;
-	int c;
-	int magic[4];
-
-	printf("File magic:");
-
-	for (i=0; i<4; i++) {
-		magic[i] = getc(inFile);
-		if (magic[i] == EOF) {
-			printf("EOF not swf\n");
-			exit(1);
-		}
-		printf(" 0x%x", magic[i]);
-	}
-	printf("\n");
-	
-	if ( (magic[1] != 87) || (magic[2] != 83) ) {
-		printf("Not a swf file '*WS'\n");
-		exit(1);
-	}
-
-	switch (magic[0]) {
-		case 67:
-			printf("zlib compressed\n");
-			break;
-		case 90:
-			printf("LZMA compressed\n");
-			break;
-		case 70:
-			printf("Not compressed\n");
-			break;
-		default:
-			printf("Not a swf file, invalid compression\n");
-			exit(1);
-	}
-
-	printf("Version: %d\n",magic[3]);
-
-	return magic[0];
-}
-
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	int compressionType;
-	int fileLength;
+	char *begOfBuf;
+	char *endOfBuf;
+	unsigned long fileLen;
 
 	// no file provided
-	if (argc != 2) {
+	if (argc !=2) {
 		printf("Just one file...\n");
-		return 1;
+		exit(1);
 	}
-	
 	else {
-		if ((fp = fopen(argv[1], "r")) == NULL) {
+		if (( fp = fopen(argv[1], "r")) == NULL) {
 			printf("Can not open file %s\n", argv[1]);
-			return 1;
+			exit(1);
 		}
 		else {
-			compressionType = idFile(fp);
-			printf("Compression type = 0x%x\n", compressionType);
+			// get the file length
+			fseek(fp, 0, SEEK_END);
+			fileLen = ftell(fp);
+			printf("File size on disk: %lu\n", fileLen);
+			fseek(fp, 0, SEEK_SET);
+			
+			// 8 bytes are required for the header
+			if (fileLen < 8) {
+				printf("Incomplete header\n");
+				exit(1);
+			}
+
+			// allocate buffer
+			begOfBuf = malloc(fileLen + 1);
+			if (begOfBuf == NULL) {
+				printf("Allocation failed\n");
+				exit(1);
+			}
+			else {
+			// fill the buffer
+			fread(begOfBuf, 1, fileLen, fp);
+			}
+			endOfBuf = begOfBuf + fileLen;
+			//printf("first byte 0x%x\n", *begOfBuf);
+			//printf("second byte 0x%x\n", *(begOfBuf+1));
+			//printf("third byte 0x%x\n", *(begOfBuf+2));
 		}
+	}	
+
+	// identify the file
+	if ( (*(begOfBuf+1) != 0x57) || (*(begOfBuf+2)) != 0x53 ) {
+		printf("File is not SWF *WS\n");
+		exit(1);
 	}
 
-	// Verify the file length
-	// length is 4-byte
-	
+	// Identify the compression type
+	switch (*begOfBuf) {
+		case 0x46:
+			printf("Not compressed\n"); 
+			// length is 4-bytes following FWS
+			break;
+		case 0x43:
+			printf("zlib compressed\n");
+			break;
+		case 0x5A:
+			printf("LZMA compressed\n");
+			break;
+		default:
+			printf("Not a SWF, invalid compression type\n");
+			break;
+	}
 
-	// Done
-	fclose(fp);
-	return 0;
+	// Identify the version
+	printf("Version: %d\n", *(begOfBuf+3));
 }
